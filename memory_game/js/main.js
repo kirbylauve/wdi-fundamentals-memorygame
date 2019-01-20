22c403d71f40ec8b52b54d1e55c77398e20fc1ce
Kirby Lockard <kirby@Kirbys-MacBook-Pro.local>
2019-01-20 12:18:00 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 console.log("Up and Running!");
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+  	rank: 'queen',
+  	suit: 'hearts',
+  	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -11,11 +32,13 @@ alert("Sorry, try again.");
 }
 }
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2){
 		checkForMatch()
 		}
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 flipCard(0);
 flipCard(2);
