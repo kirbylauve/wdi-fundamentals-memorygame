09e5b243ae2b38f8c58bc1f5a085c3c772823cbd
Kirby Lockard <kirby@Kirbys-MacBook-Pro.local>
2019-01-20 20:31:23 -0500

    created game logic - broken

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,15 +31,30 @@ alert("You found a match!");
 alert("Sorry, try again.");
 }
 }
-var flipCard = function(cardId){
+var flipCard = function(cardElement){
+	this.getAttribute('data-id', 'cardId');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute(cards[cardId],cardImage);
 	if (cardsInPlay.length === 2){
 		checkForMatch()
 		}
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 }
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+	for (var i = 0; i< cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('cardElement', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		cardElement.appendChild('game-board')
+	}
+}
+
+
+createBoard();
+
+
 
